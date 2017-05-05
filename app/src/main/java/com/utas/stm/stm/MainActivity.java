103065c009ff52fa9117503c64a3dfcfc1651e26
drea0 <drea0@utas.edu.au>
2017-05-05 21:09:10 +1000

    Added empty view activity for testing. Record button now successfully opens a new activity.

--- app/src/main/java/com/utas/stm/stm/MainActivity.java
+++ app/src/main/java/com/utas/stm/stm/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void editActivity(View v) {
-        startActivity(new Intent(MainActivity.this, EditActivity.class));
+        startActivity(new Intent(this, EditActivity.class));
     }
+
 }
