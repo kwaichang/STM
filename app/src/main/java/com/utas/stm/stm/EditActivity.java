7a5aa5db6d278f97c018cb6aa56794abc758da2f
kwaichang <drooghead@gmail.com>
2017-05-14 02:52:15 +1000

    Added date picker

--- app/src/main/java/com/utas/stm/stm/EditActivity.java
+++ app/src/main/java/com/utas/stm/stm/EditActivity.java
@@ -1,22 +1,32 @@
 package com.utas.stm.stm;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.icu.text.AlphabeticIndex;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
-import android.app.Activity;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
         // Get a Spinner and bind it to an ArrayAdapter that
         // references a String array.
         Spinner importanceSpinner = (Spinner) findViewById(R.id.editImportanceSpinner);
@@ -24,5 +34,50 @@ public class EditActivity extends AppCompatActivity {
                 this, R.array.importance_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         importanceSpinner.setAdapter(adapter);
+
+              dateView = (TextView) findViewById(R.id.editDueDate);
+      calendar = Calendar.getInstance();
+      year = calendar.get(Calendar.YEAR);
+
+      month = calendar.get(Calendar.MONTH);
+      day = calendar.get(Calendar.DAY_OF_MONTH);
+      showDate(year, month+1, day);
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
     }
 }
+
