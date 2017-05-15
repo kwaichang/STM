8d8619cf970c1c045244123dbce98756f098c5b6
drea0 <drea0@utas.edu.au>
2017-05-15 17:08:01 +1000

    Added different date and time pickers. Both are working well

--- app/src/main/java/com/utas/stm/stm/EditActivity.java
+++ app/src/main/java/com/utas/stm/stm/EditActivity.java
@@ -2,6 +2,7 @@ package com.utas.stm.stm;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.icu.text.AlphabeticIndex;
 import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,74 +11,86 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class EditActivity extends AppCompatActivity {
-    private DatePicker datePicker;
-    private Calendar calendar;
-    private TextView dateView;
-    private int year, month, day;
+public class EditActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnDatePicker, btnTimePicker, btnSubmit;
+    EditText txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
-        // Get a Spinner and bind it to an ArrayAdapter that
-        // references a String array.
-        Spinner importanceSpinner = (Spinner) findViewById(R.id.editImportanceSpinner);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(
-                this, R.array.importance_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        importanceSpinner.setAdapter(adapter);
+        btnDatePicker=(Button)findViewById(R.id.editSetDateBtn);
+        btnTimePicker=(Button)findViewById(R.id.editSetTimeBtn);
+        btnSubmit=(Button)findViewById(R.id.editSubmitBtn);
+        txtDate=(EditText)findViewById(R.id.editDueDate);
+        txtTime=(EditText)findViewById(R.id.editDueTime);
 
-              dateView = (TextView) findViewById(R.id.editDueDate);
-      calendar = Calendar.getInstance();
-      year = calendar.get(Calendar.YEAR);
+        btnDatePicker.setOnClickListener(this);
+        btnTimePicker.setOnClickListener(this);
+        btnSubmit.setOnClickListener(this);
 
-      month = calendar.get(Calendar.MONTH);
-      day = calendar.get(Calendar.DAY_OF_MONTH);
-      showDate(year, month+1, day);
-    }
-
-    @SuppressWarnings("deprecation")
-    public void setDate(View view) {
-        showDialog(999);
-        Toast.makeText(getApplicationContext(), "ca",
-                Toast.LENGTH_SHORT)
-                .show();
     }
 
     @Override
-    protected Dialog onCreateDialog(int id) {
-        // TODO Auto-generated method stub
-        if (id == 999) {
-            return new DatePickerDialog(this,
-                    myDateListener, year, month, day);
+    public void onClick(View v) {
+
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
         }
-        return null;
+        if (v == btnTimePicker) {
+            Log.d("submit", "time set button pressed");
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            txtTime.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
+
+        if (v == btnSubmit) {
+            Log.d("submit", "submit button pressed");
+        }
     }
 
-    private DatePickerDialog.OnDateSetListener myDateListener = new
-            DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker arg0,
-                                      int arg1, int arg2, int arg3) {
-                    // TODO Auto-generated method stub
-                    // arg1 = year
-                    // arg2 = month
-                    // arg3 = day
-                    showDate(arg1, arg2+1, arg3);
-                }
-            };
 
-    private void showDate(int year, int month, int day) {
-        dateView.setText(new StringBuilder().append(day).append("/")
-                .append(month).append("/").append(year));
-    }
 }
-
