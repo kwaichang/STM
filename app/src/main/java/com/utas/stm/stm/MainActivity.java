bd90ce1e4d74d6e391d257fef0502564316ec7df
whoaie <nickstevenson1994@gmail.com>
2017-05-19 19:26:55 +1000

    Finished Questionnaire
    Everything works, score is calculated correctly but only displays on the
    questionnaire activity, soon to change though!
    
    Still working on the list.

--- app/src/main/java/com/utas/stm/stm/MainActivity.java
+++ app/src/main/java/com/utas/stm/stm/MainActivity.java
@@ -5,15 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void editActivity(View v) {
@@ -22,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
     public void viewActivity(View v) {
         startActivity(new Intent(this, ViewActivity.class));
     }
+    public void questionnaireActivity(View v) { startActivity(new Intent(this, Questionnaire.class));}
+
 
 }
---
+++ app/src/main/java/com/utas/stm/stm/Questionnaire.java
@@ -0,0 +1,53 @@
+package com.utas.stm.stm;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.content.res.Resources;
+import android.content.Intent;
+
+public class Questionnaire extends Activity implements View.OnClickListener {
+
+    Button btnSubmit;
+    RadioGroup radioGroup;
+    float total = 0;
+    Intent intent = new Intent();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_questionnaire);
+
+        btnSubmit=(Button)findViewById(R.id.quizSubmitBtn);
+
+        btnSubmit.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+
+        if (v == btnSubmit){
+            for(int i = 1; i < 19; i++)
+            {
+                String radioID = "quest" + i;
+                int resID = getResources().getIdentifier(radioID, "id", getPackageName());
+                RadioGroup radioGroup = (RadioGroup)findViewById(resID);
+                int radioButtonID = radioGroup.getCheckedRadioButtonId();
+                View radioButton = radioGroup.findViewById(radioButtonID);
+                int idx = radioGroup.indexOfChild(radioButton);
+                idx++;
+                total = total + idx;
+            }
+            total = total/90;
+            total *= 100;
+            int totalint = (int) Math.round(total);
+            btnSubmit.setText("Score: "+totalint+"%");
+            intent.putExtra("score", totalint);
+            total = 0;
+        }
+    }
+
+}
